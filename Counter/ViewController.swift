fe04187c8389a71d9a7c9a72d4cf2867432f2307
Aleksandr <125129763+Kosmonur@users.noreply.github.com>
2023-02-17 23:40:29 +0300

    Финальная версия после правок по замечаниям ревьюера

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,20 +8,51 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController, AVAudioPlayerDelegate {
-    
-    // задаём начальное значение счётчика
-    private var counter:UInt = 0
+class ViewController: UIViewController {
     
     // добавим Haptic Touch на кнопку - определим силу отклика
     private let rigidImpactFeedbackGenerator = UIImpactFeedbackGenerator(style: .rigid)
+    
+    // задаём начальное значение счётчика
+    private var counter:UInt = 0
     
     // добавим в программу аудио
     private var audioPlayers: [AVAudioPlayer] = []
     private var audioPlayersLock = NSRecursiveLock()
     
+    /// Функция вызывается один раз при начальной загрузке программы
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // выводим значенние счётчика
+        counterLabel.text = "Значение счётчика:\n\n\(counter)"
+        // делаем кнопку круглой
+        counterButton.layer.cornerRadius = 0.5 * counterButton.bounds.size.height
+    }
+        
+    @IBOutlet private weak var counterLabel: UILabel!
+    @IBOutlet private weak var counterButton: UIButton!
+      
+    @IBAction private func buttonTouchDown(_ sender: Any) {
+        // при нажатии кнопки - делаем Haptic Touch и играем звук
+        rigidImpactFeedbackGenerator.impactOccurred()
+        playButtonSound(.soundButtonDown)
+    }
+    
+    @IBAction private func buttonTouchUpInside(_ sender: Any) {
+        // при отпускании кнопки - инкрементим счетчик с проверкой на переполнение, делаем Haptic Touch и играем звук
+        
+        counter = counter < UInt.max ? counter + 1 : 0
+        counterLabel.text = "Значение счётчика:\n\n\(counter)"
+        
+        rigidImpactFeedbackGenerator.impactOccurred()
+        playButtonSound(.soundButtonUp)
+    }
+}
+
+extension ViewController: AVAudioPlayerDelegate {
+    
     // будет два разных звука - один для нажатия кнопки, второй - для отпускания
-    enum ButtonSound {
+    private enum ButtonSound {
         case soundButtonUp
         case soundButtonDown
         
@@ -29,7 +60,6 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
             switch self {
             case .soundButtonUp:
                 return "soundbuttonup"
-
             case .soundButtonDown:
                 return "soundbuttondown"
             }
@@ -39,44 +69,16 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
             Bundle.main.url(forResource: self.fileName, withExtension: "wav")
         }
     }
-    
-    @IBOutlet weak var counterLabel: UILabel!
-    @IBOutlet weak var counterButton: UIButton!
-    
-    /// Функция вызывается один раз при начальной загрузке программы
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // выводим значенние счётчика
-        counterLabel.text = "Значение счётчика:\n\n\(counter)"
-        // делаем кнопку круглой
-        counterButton.layer.cornerRadius = 0.5*counterButton.bounds.size.height      
-    }
-    
-    @IBAction func buttonTouchDown(_ sender: Any) {
-        // при нажатии кнопки - делаем Haptic Touch и играем звук
-        rigidImpactFeedbackGenerator.impactOccurred()
-        playButtonSound(.soundButtonDown)
-    }
-    
-    @IBAction func buttonTouchUpInside(_ sender: Any) {
-        // при отпускании кнопки - инкрементим счетчик с проверкой на переполнение, делаем Haptic Touch и играем звук
-        
-        counter = counter < UInt.max ? counter + 1 : 0
-        counterLabel.text = "Значение счётчика:\n\n\(counter)"
-        
-        rigidImpactFeedbackGenerator.impactOccurred()
-        playButtonSound(.soundButtonUp)
-    }
-    
+          
     /// Функция воспроизведения звуков
-    func playButtonSound(_ buttonSound: ButtonSound) {
+    private func playButtonSound(_ buttonSound: ButtonSound) {
         guard let buttonSoundURL = buttonSound.fileURL else {
             print("Ошибка в url на звук \(buttonSound.fileName)")
             return
         }
 
         DispatchQueue.global(qos: .userInitiated).async { [weak self] in
-            guard let self = self else { return }
+            guard let self else { return }
             do {
                 let audioPlayer = try AVAudioPlayer(contentsOf: buttonSoundURL)
                 audioPlayer.delegate = self
@@ -84,7 +86,6 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
                 self.audioPlayersLock.lock()
                 self.audioPlayers.append(audioPlayer)
                 self.audioPlayersLock.unlock()
-
                 // audioPlayer.volume = 1
                 audioPlayer.prepareToPlay()
                 audioPlayer.play()
@@ -97,7 +98,7 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
     /// Функция вызывается, когда проигрыватель завершил воспроизведение звука
     func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
         audioPlayersLock.lock()
-        self.audioPlayers = self.audioPlayers.filter { $0 !== player }
+        audioPlayers = audioPlayers.filter { $0 !== player }
         audioPlayersLock.unlock()
     }
 }
